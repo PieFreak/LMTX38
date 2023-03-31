c094269f482a4917d4fd1938d4f9609f7cdaf52c
JackS15 <stahlja@chalmers.se>
2023-03-31 09:47:38 +0200

    result and pdf reader test

--- client/src/components/SettingPopup.js
+++ client/src/components/SettingPopup.js
@@ -20,6 +20,7 @@ const SettingPopup = ({ title, text, show, onClose }) => {
                 questionType: title,
                 questionAmount: amount
             });
+            /* console.log(response.data); */
             navigate('/gameplay', { state: { game: response.data } });
         } catch (error) {
             console.error(error.response.data);
@@ -30,20 +31,20 @@ const SettingPopup = ({ title, text, show, onClose }) => {
 
     return (
         <div onClick={() => { onClose(); setSelectedButton(20); }} className="bg-gray-800 bg-opacity-30 fixed flex justify-center items-center w-full h-full">
-            <div onClick={(e) => { e.stopPropagation(); }} className="w-11/12 max-w-3xl top-20 md:top-56 shadow-2xl shadow-black h-auto md:h-96 bg-indigo-50 rounded-2xl fixed flex items-center justify-center">
-                <button onClick={() => { onClose(); setSelectedButton("20"); }} className="absolute rounded-2xl top-0 left-0 pt-2 px-2 hover:bg-inherit hover:border-2 hover:shadow-black hover:shadow-2xl">X</button>
+            <div onClick={(e) => { e.stopPropagation(); }} className="w-[16rem] border-8 border-indigo-100 md:mx-auto lg:mx-auto md:w-[36rem] lg:w-[40rem] max-w-3xl top-20 md:top-56 shadow-2xl shadow-black h-auto md:h-96 bg-gradient-to-b from-white via-[#f2edf1] to-indigo-50 fixed flex items-center justify-center">
+                <button onClick={() => { onClose(); setSelectedButton("20"); }} className="absolute top-0 left-0 pt-1 px-2 hover:bg-inherit hover:shadow-inner">X</button>
                 <div className="h-full">
                     <h1 className="text-3xl mt-8 mb-4 text-center">{title}</h1>
-                    <h3 className="text-sm md:text-xl mx-8 md:mx-32 mb-8 text-center">{text}</h3>
+                    <h3 className="text-sm md:text-xl mx-4 md:mx-32 mb-4 md:mb-8 text-center">{text}</h3>
                     <p className=" my-4 text-xs text-center">Välj antal frågor omgången ska innehålla</p>
-                    <div className="mx-auto flex flex-row items-center justify-center space-x-2 md:space-x-10 mb-4">
+                    <div className="mx-auto flex flex-row items-center justify-center space-x-2 md:space-x-10 my-2">
                         <button className={`text-xs md:text-base border-2 px-1 md:px-4 md:m-2 ${selectedButton === 5 ? 'bg-indigo-200' : ' bg-white'}`} onClick={() => handleAmountOption(5)}>5</button>
                         <button className={`text-xs md:text-base border-2 px-1 md:px-4 md:m-2 ${selectedButton === 10 ? 'bg-indigo-200' : ' bg-white'}`} onClick={() => handleAmountOption(10)}>10</button>
                         <button className={`text-xs md:text-base border-2 px-1 md:px-4 md:m-2 ${selectedButton === 20 ? 'bg-indigo-200' : ' bg-white'}`} onClick={() => handleAmountOption(20)}>20</button>
                         <button className={`text-xs md:text-base border-2 px-1 md:px-4 md:m-2 ${selectedButton === 30 ? 'bg-indigo-200' : ' bg-white'}`} onClick={() => handleAmountOption(30)}>30</button>
                         <button className={`text-xs md:text-base border-2 px-1 md:px-4 md:m-2 ${selectedButton === 40 ? 'bg-indigo-200' : ' bg-white'}`} onClick={() => handleAmountOption(40)}>40</button>
                     </div>
-                    <button onClick={startGame} className="mb-2 mx-auto flex justify-center items-center w-24 md:w-40 border-2 bg-[#ffffff] hover:bg-[#75969a50] border-indigo-200 transition-all duration-300 rounded-md">
+                    <button onClick={startGame} className="mx-auto mb-2 md:mt-6 lg:mt-10 flex justify-center items-center w-24 md:w-40 border-2 bg-[#ffffff] hover:bg-[#a8dfe618] border-indigo-200 transition-all duration-300 rounded-md">
                         <h2 className="font-extrabold text-small md:text-base m-2 md:m-4 anim-textAppear text-[#ac1d1d] ">Starta</h2>
                     </button>
                 </div>
--- client/src/pages/Result.js
+++ client/src/pages/Result.js
@@ -4,40 +4,157 @@ import Footer from '../components/Footer';
 import { useLocation } from 'react-router-dom';
 
 export default function Result() {
+
     const location = useLocation();
     const [roundID, setRoundID] = useState();
     const [game, setGame] = useState();
-    const [answered, setAnswered] = useState();    
+    const [answered, setAnswered] = useState();
+    const [score, setScore] = useState();
+    const [expand, setExpand] = useState([]);
+    console.log(answered);
+    const handleExpand = (index) => {
+        setExpand((prevState) => {
+            const newState = [...prevState];
+            newState[index] = !newState[index];
+            return newState;
+        });
+    };
 
-    useEffect(()=> {
+    useEffect(() => {
         setRoundID(location.state.roundID);
         setGame(location.state.game);
         setAnswered(location.state.answered);
+        setScore(location.state.score);
     }, []);
+
+    const findCorrectAnswerLetter = (answers) => {
+        const correctAnswerIndex = answers.findIndex((answer) => answer.correct);
+        return String.fromCharCode(65 + correctAnswerIndex);
+    };
+
+
     return (
         <div className="min-h-screen flex flex-col justify-center item-center bg-indigo-5">
-            <Navbar/>
-            <div className="w-full mx-auto bg-[#5b6c6c] pb-16 px-4">
-                <h1 className="pt-12 mb-16 flex justify-center items-center w-full text-white text-2xl md:text-4xl p-2">RESULTAT</h1>
-                <div className="flex flex-col text-white gap-2">
-                    {game.map(question => {
+            <Navbar />
+            <div className="w-full mx-auto bg-gradient-to-b from-white via-[#f2edf1a9] to-[#a3c0c5] bg-opacity-50 pb-16 px-4">
+                <h1 className="pt-16 mb-8 flex justify-center items-center w-full text-black text-2xl md:text-5xl p-2">RESULTAT</h1>
+                <h1 className="mx-auto text-center pb-12 text-black text-sm md:text-xl">Du fick {score}/{game && game.length} rätt</h1>
+
+                <div className="flex flex-col justify-center items-center text-white">
+                    <div className="w-[16rem] md:w-[36rem] grid grid-cols-7">
+                        <h1 className="text-center font-extralight col-start-7 col-end-8 text-black text-[0.6rem] md:text-xs">Ditt svar</h1>
+                    </div>
+                    {game && answered && game.map((question, index) => {
                         return (
-                            <button className='flex border-2'>
-                                <span>Question: {question.question}</span>
-                                {question.answers.map((answer, index) => {
-                                    return (
-                                        <span className={`pl-2 ${answer.correct && "font-bold"}`}><br/>Answer {index+1}: {answer.answer}</span>
-                                    )
-                                })}
+                            <button className={`hover:shadow-sm  ${!expand[index] && answered[index].correct === 1 && "hover:shadow-green-400 shadow-green-900"} ${!expand[index] && answered[index].correct === 0 && "hover:shadow-red-400  shadow-red-900"} w-[16rem] md:w-[36rem] text-black my-1 ${expand[index] ? " cursor-default border-b-none border-x-2 border-b-4 pb-2" : "border-b-2 border-x-2 borde"}`}
+                                onClick={() => handleExpand(index)}
+                                key={index}>
+                                <div className="grid grid-cols-7">
+                                    <h1 className="col-start-1 col-end-2 text-start pl-2 text-[1.2rem] py-1">{index + 1}. </h1>
+                                    <h1 className={`col-start-2 col-end-7 text-start self-center text-[0.6rem] md:text-[0.7rem] py-1 ${!expand[index] ? 'md:max-w-[14rem] overflow-hidden whitespace-nowrap text-ellipsis' : ''
+                                        }`}>{question.question} </h1>
+                                    {!expand[index] ? (
+                                        <div className={`col-start-7 col-end-8 px-4 flex items-center justify-center bg-white ${answered[index].correct === 1 ? " bg-[#2078456b]" : " bg-[#bb171794]"}`}>
+                                            {!expand[index] && findCorrectAnswerLetter(question.answers)}
+                                        </div>
+                                    ) : null}
+
+                                </div>
+                                {expand[index] &&
+                                    <div className="w-full">
+                                        {question.answers.map((answer, answerIndex) => {
+                                            return (
+                                                <div className={`grid grid-cols-8 w-full mx-auto py-1 my-4 ${answered[index].correct === 0 && answered[index].answer === answer.answer && "bg-red-200 bg-opacity-30"} ${answer.correct && "shadow-lg shadow-[#9cd3a2bf] border-x-2 border-x-[#9cd3a2bf] font-bold"}`} key={answerIndex}>
+                                                    <p className="col-start-1 col-end-2 self-center text-[0.57rem] md:text-[0.6rem]">{String.fromCharCode(65 + answerIndex)}. </p>
+                                                    <p className={`col-start-2 col-end-8 font-light text-[0.57rem] md:text-[0.6rem] text-center pl-2 items-center`} >{answer.answer}</p>
+                                                </div>)
+                                        })}
+                                    </div>
+                                }
                             </button>
                         )
                     })}
                 </div>
 
-                
+                <div className="flex flex-col justify-center items-center m-10 mx-auto">
+                    <h1 className="pt-12 mt-6 mb-8 text-black text-2xl md:text-4xl p-2">UTMANA EN VÄN</h1>
+                    <h1 className="text-center pb-12 text-black text-[0.3.rem] mx-8 md:text-xl">Dela koden nedan och se vem som får flest poäng!</h1>
+                    <p className="text-xs text-center">{roundID}</p>
+                </div>
 
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 }
+
+
+
+
+
+
+
+
+/* export default function Result() {
+
+    const location = useLocation();
+    const [roundID, setRoundID] = useState();
+    const [game, setGame] = useState();
+    const [answered, setAnswered] = useState();
+    const [score, setScore] = useState();
+    const [expand, setExpand] = useState([]);
+    console.log(answered);
+    const handleExpand = (index) => {
+        setExpand((prevState) => {
+            const newState = [...prevState];
+            newState[index] = !newState[index];
+            return newState;
+        });
+    };
+
+    useEffect(() => {
+        setRoundID(location.state.roundID);
+        setGame(location.state.game);
+        setAnswered(location.state.answered);
+        setScore(location.state.score);
+    }, []);
+
+    return (
+        <div className="min-h-screen flex flex-col justify-center item-center bg-indigo-5">
+            <Navbar />
+            <div className="w-full mx-auto bg-white bg-opacity-50 pb-16 px-4">
+                <h1 className="pt-12 mb-8 flex justify-center items-center w-full text-black text-2xl md:text-6xl p-2">RESULTAT</h1>
+                <h1 className="mx-auto text-center pb-12 text-black text-base md:text-xl">{score}/{game && game.length}</h1>
+
+                <div className="flex flex-col justify-center items-center text-white gap-2">
+
+
+                    {game && answered && game.map((question, index) => {
+                        return (
+                            <button className={`hover:shadow-sm  ${!expand[index] && answered[index].correct === 1 && "hover:shadow-green-400 shadow-green-900 bg-[#1b9a5011]"} ${!expand[index] && answered[index].correct === 0 && "hover:shadow-red-400 shadow-red-900 bg-[#9a1b1b11]"} w-[30rem] text-black my-2 ${expand[index] ? " cursor-default border-b-none border-x-2 border-b-4 pb-4" : "border-b-2 border-x-2"}`}
+                                onClick={() => handleExpand(index)}
+                                key={index}>
+                                <h1 className="self-center text-start pl-2 text-[0.6rem] md:text-[0.7rem] py-1">{index + 1}. {question.question}</h1>
+                                {expand[index] &&
+                                    <div className="w-full">
+                                        {question.answers.map((answer, answerIndex) => {
+                                            return (
+                                                <div className={`grid grid-cols-8 w-full mx-auto py-1 my-4 ${answered[index].correct === 0 && answered[index].answer === answer.answer && "bg-red-200 bg-opacity-30"} ${answer.correct && "shadow-lg shadow-[#9cd3a2bf] border-x-2 border-x-[#9cd3a2bf] font-bold"}`} key={answerIndex}>
+                                                    <p className="col-start-1 col-end-2 self-center text-[0.57rem] md:text-[0.6rem]">{String.fromCharCode(65 + answerIndex)}. </p>
+                                                    <p className={`col-start-2 col-end-8 font-light text-[0.57rem] md:text-[0.6rem] text-center pl-2 items-center`} >{answer.answer}</p>
+                                                </div>)
+                                        })}
+                                    </div>
+                                }
+
+                            </button>
+                        )
+                    })}
+
+
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
+} */
